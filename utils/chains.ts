f81b22e6be6ab5be05a74425ac337f36b3770a79
automagicknight <147991552+automagicknight@users.noreply.github.com>
2024-10-02 22:00:37 +0200

    Initialize feat/toucan-jordan

---
+++ utils/chains.ts
@@ -0,0 +1,68 @@
+import {
+  polygon,
+  mainnet,
+  sepolia,
+  arbitrum,
+  arbitrumSepolia,
+  polygonMumbai,
+  Chain,
+  optimismSepolia,
+} from "@wagmi/core/chains";
+
+const chainNames = [
+  "mainnet",
+  "polygon",
+  "sepolia",
+  "mumbai",
+  "arbitrum",
+  "arbitrumSepolia",
+  "optimismSepolia",
+] as const;
+export type ChainName = (typeof chainNames)[number];
+
+export function readableChainName(chainName: ChainName): string {
+  switch (chainName) {
+    case "mainnet":
+      return "Ethereum";
+    case "polygon":
+      return "Polygon";
+    case "sepolia":
+      return "Sepolia";
+    case "mumbai":
+      return "Polygon Mumbai";
+    case "arbitrum":
+      return "Arbitrum One";
+    case "arbitrumSepolia":
+      return "Arbitrum Sepolia";
+    case "optimismSepolia":
+      return "Optimism Sepolia";
+    default:
+      throw new Error("Unknown chain");
+  }
+}
+
+// as const prevents using includes check
+const testnets = ["sepolia", "mumbai", "arbitrumSepolia", "optimismSepolia"];
+
+export function getChain(chainName: ChainName): Chain {
+  switch (chainName) {
+    case "mainnet":
+      return mainnet;
+    case "polygon":
+      return polygon;
+    case "arbitrum":
+      return arbitrum;
+    case "sepolia":
+      return sepolia;
+    case "arbitrumSepolia":
+      return arbitrumSepolia;
+    case "mumbai":
+      return polygonMumbai;
+    case "optimismSepolia":
+      return optimismSepolia;
+    default:
+      throw new Error("Unknown chain");
+  }
+}
+
+export const isTestnet = (chainName: ChainName) => testnets.includes(chainName);
