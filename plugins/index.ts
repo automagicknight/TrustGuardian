a3fa9ddcfbf66fb5800e5e434ffa161518f83e96
automagicknight <147991552+automagicknight@users.noreply.github.com>
2024-10-02 22:00:39 +0200

    Initialize wip/temp-backport

---
+++ plugins/index.ts
@@ -0,0 +1,65 @@
+import {
+  PUB_DELEGATION_WALL_CONTRACT_ADDRESS,
+  PUB_DUAL_GOVERNANCE_PLUGIN_ADDRESS,
+  PUB_MULTISIG_PLUGIN_ADDRESS,
+  PUB_EMERGENCY_MULTISIG_PLUGIN_ADDRESS,
+  // PUB_TOKEN_VOTING_PLUGIN_ADDRESS,
+  // PUB_LOCK_TO_VOTE_PLUGIN_ADDRESS,
+} from "@/constants";
+import { IconType } from "@aragon/ods";
+
+type PluginItem = {
+  /** The URL fragment after /plugins */
+  id: string;
+  /** The name of the folder within `/plugins` */
+  folderName: string;
+  /** Title on menu */
+  title: string;
+  icon?: IconType;
+  pluginAddress: string;
+};
+
+export const plugins: PluginItem[] = [
+  // {
+  //   id: "token-voting",
+  //   folderName: "tokenVoting",
+  //   title: "Community proposals",
+  //   icon: IconType.BLOCKCHAIN_BLOCKCHAIN,
+  //   pluginAddress: PUB_TOKEN_VOTING_PLUGIN_ADDRESS,
+  // },
+  {
+    id: "optimistic",
+    folderName: "optimistic-proposals",
+    title: "Proposals",
+    // icon: IconType.APP_MEMBERS,
+    pluginAddress: PUB_DUAL_GOVERNANCE_PLUGIN_ADDRESS,
+  },
+  // {
+  //   id: "lock-to-vote",
+  //   folderName: "lockToVote",
+  //   title: "Lock to bote",
+  //   icon: IconType.BLOCKCHAIN_BLOCK,
+  //   pluginAddress: PUB_LOCK_TO_VOTE_PLUGIN_ADDRESS,
+  // },
+  {
+    id: "multisig",
+    folderName: "multisig",
+    title: "Multisig",
+    // icon: IconType.BLOCKCHAIN_BLOCKCHAIN,
+    pluginAddress: PUB_MULTISIG_PLUGIN_ADDRESS,
+  },
+  {
+    id: "emergency",
+    folderName: "emergency-multisig",
+    title: "Security Council",
+    // icon: IconType.BLOCKCHAIN_BLOCKCHAIN,
+    pluginAddress: PUB_EMERGENCY_MULTISIG_PLUGIN_ADDRESS,
+  },
+  {
+    id: "members",
+    folderName: "members",
+    title: "Members",
+    // icon: IconType.BLOCKCHAIN_BLOCKCHAIN,
+    pluginAddress: PUB_DELEGATION_WALL_CONTRACT_ADDRESS,
+  },
+];
